de7b3745fc407974fb34c9e73d2d85f177254cd9
Adebola <inocvile@gmail.com>
2020-04-07 04:18:05 +0000

    basic css changes

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,9 +10,14 @@ import PropTypes from "prop-types"
 
 import "./layout.css"
 import "bootstrap/dist/css/bootstrap.min.css"
-// import "./bootstrap.min.css"
+import { Navbar } from "./Globals/Navbar"
 
-const Layout = ({ children }) => <>{children}</>
+const Layout = ({ children }) => (
+  <>
+    <Navbar />
+    {children}
+  </>
+)
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
